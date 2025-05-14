04ca396d925bbde1cae63e01f81aa9c9ba3ad526
ts0ra <gaming.heroes354@gmail.com>
2025-05-14 14:58:34 +0700

    feat(home): make quantity use select button instead increment 1 by 1

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach(product => {
       </div>
 
       <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
           <option selected value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -59,7 +59,9 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 document.querySelectorAll('.js-add-to-cart').forEach(button => {
   button.addEventListener('click', () => {
     const productId = button.dataset.productId;
-
+    const selectorQuantity = document.querySelector(`.js-quantity-selector-${productId}`);
+    const quantity = Number(selectorQuantity.value);
+    
     let matchingItem;
 
     cart.forEach(item => {
@@ -69,11 +71,11 @@ document.querySelectorAll('.js-add-to-cart').forEach(button => {
     });
 
     if (matchingItem) {
-      matchingItem.quantity += 1;
+      matchingItem.quantity += quantity;
     } else {
       cart.push({
         productId: productId,
-        quantity: 1
+        quantity: quantity
       });
     }
 
