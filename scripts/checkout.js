6d254aa1622adcc0ac0b771d6e4c62cc78e8fc34
ts0ra <gaming.heroes354@gmail.com>
2025-05-14 17:52:53 +0700

    feat(checkout): delete products from HTML when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -16,7 +16,7 @@ cart.forEach(cartItem => {
   });
 
   cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
         Delivery date: Tuesday, June 21
       </div>
@@ -104,6 +104,9 @@ document
     link.addEventListener('click', () => {
       const productId = link.dataset.productId;
       removeFromCart(productId);
-      console.log(cart);
+      
+      const container = document.querySelector(`.js-cart-item-container-${productId}`);
+
+      container.remove();
     });
 });
