effae4de33cb4e80745e57d9bf4860633aa86e4d
ts0ra <gaming.heroes354@gmail.com>
2025-05-16 14:23:11 +0700

    rename tests0jasmine folder to tests

--- tests/tests-simple/moneyTest.js
+++ tests/tests-simple/moneyTest.js
@@ -1,4 +1,4 @@
-import formatCurrency from "../scripts/utils/money.js";
+import formatCurrency from "../../scripts/utils/money.js";
 
 console.log('test suite: formatCurreny');
 console.log('converts cents into dollars');
